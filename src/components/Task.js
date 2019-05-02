c869908070afdcdf1153ed1cf04a095aa93634dc
jeremy <jeremyjames411@gmail.com>
2019-05-02 13:17:04 -0400

    Update - the look of the remove task button

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,12 +3,10 @@ import React from 'react';
 const Task = (props) => {
   const { task, index } = props;
   return (
-    <div>
-      <div className={task.done ? 'completed' : ''} key={index}>
+    <div className={task.done ? 'completed' : ''} key={index}>
       <input onChange={(event) => props.toggleTaskCompleted(event, index)} type="checkbox" checked={task.done} />
-        {task.title}
-      </div>
-      <button onClick={() => props.removeTask(index)}>Remove Task</button>
+      {task.title}
+      <button onClick={() => props.removeTask(index)}>X</button>
     </div>
   )
 }
