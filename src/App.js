a85d5606ee7003d15f408d0a006036f4e3cfad7a
jeremy <jeremyjames411@gmail.com>
2019-05-02 13:01:21 -0400

    Add - Organization of components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Header from './Header';
+import Header from './components/Header';
+import Main from './components/Main';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Main />
       </div>
     );
   }
