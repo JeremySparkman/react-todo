8ae008329863c641abb17d24da8f3bb3dcb47604
jeremy <jeremyjames411@gmail.com>
2019-05-02 13:03:50 -0400

    Update - Main component and reorganize

---
+++ src/components/Main.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Button from './Button';
+import TasksList from './TasksList';
+
+class Main extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      newTask : '',
+      tasks : []
+    };
+  
+  }
+  newTaskChanged(e){
+    this.setState({
+        newTask: e.target.value
+    });
+  }
+
+  addTask(e) {
+    e.preventDefault();
+    this.setState({
+      newTask: '',
+      tasks : [...this.state.tasks, {
+        title : this.state.newTask,
+        done: false
+      }]
+    });
+    document.getElementById("submitTask").reset();
+  }
+
+  toggleTaskCompleted(e,i){
+    const tasks = [...this.state.tasks];
+    tasks[i] = {
+      ...tasks[i],
+      done : e.target.checked
+    };
+    this.setState({
+      tasks
+    });
+  }
+
+  render(){
+    return (
+      <div className="main">
+        <h3>Next Task</h3>
+        <form id="submitTask" onSubmit={this.addTask.bind(this)}>
+          <input onChange={(event) => this.newTaskChanged(event)} id="newTask" />
+          <Button  />
+        </form>
+        <TasksList 
+          tasks={this.state.tasks} 
+          toggleTaskCompleted={this.toggleTaskCompleted.bind(this)}
+        />
+      </div>
+    )
+  }
+}
+
+export default Main;
