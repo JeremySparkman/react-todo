2e1894128475484c908217daca3631a4e9b7097c
jeremy <jeremyjames411@gmail.com>
2019-05-02 13:03:29 -0400

    Update - Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render(){
+    return (
+      <header className='header'>
+        <div className='header__title header__title--slashd'>Next</div>
+      </header>
+    )
+  }
+
+}
+
+export default Header;
